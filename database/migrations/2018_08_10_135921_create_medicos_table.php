306e93a1a4e9d3efdaf198a4252bff7de19cfa63
Antonio Alisson Almeida Queiroz <alisson.almeidaq@gmail.com>
2018-08-10 14:44:05 -0300

    Criando Models e Migrations - inicial

--- database/migrations/2018_08_10_135921_create_medicos_table.php
+++ database/migrations/2018_08_10_135921_create_medicos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateMedicosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('medicos', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('image', 100)->nullable();
-            $table->rememberToken();
-            $table->timestamps();
+            $table->integer('user_id')->usingned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('carcade');
+            $table->string("crm", 20)->nullable();
+            
         });
     }
 
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('medicos');
     }
 }
--- database/migrations/2018_08_10_143649_create_areas_medicas_table.php
+++ database/migrations/2018_08_10_143649_create_areas_medicas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAreasMedicasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('areas_medicas', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('image', 100)->nullable();
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string("area", 100)->nullable();
+            $table->text('atuacao');
+           
         });
     }
 
@@ -31,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('areas_medicas');
     }
 }
